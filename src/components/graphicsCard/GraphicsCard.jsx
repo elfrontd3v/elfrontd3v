a5f52dd5110da402e87f4ccc6b4370c55114d74a
sebastian <sebas99cano@gmail.com>
2022-08-30 14:04:28 -0500

    cards work

---
+++ src/components/graphicsCard/GraphicsCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GraphicsCard = () => {
+  return <></>;
+};
+
+export default GraphicsCard;
--- src/pages/counts/earnings/components/Earnings.jsx
+++ src/pages/counts/earnings/components/Earnings.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Card, Col, Row } from "antd";
+import GraphicsCard from "../../../../components/graphicsCard/GraphicsCard";
 
 const Earnings = () => {
-  return <div>Earnings</div>;
+  return (
+    <Card>
+      <Row gutter={[16, 16]}>
+        <Col xs={24} sm={12} md={6} lg={6} xl={6} xxl={6}>
+          <GraphicsCard />
+        </Col>
+        <Col xs={24} sm={12} md={6} lg={6} xl={6} xxl={6}></Col>
+        <Col xs={24} sm={12} md={6} lg={6} xl={6} xxl={6}></Col>
+        <Col xs={24} sm={12} md={6} lg={6} xl={6} xxl={6}></Col>
+      </Row>
+    </Card>
+  );
 };
 
 export default Earnings;
