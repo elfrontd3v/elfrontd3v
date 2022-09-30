839032c90cc2346e3bd8bf6a6c10d6cb2406e2a2
sebastian <sebas99cano@gmail.com>
2022-09-30 01:34:50 -0500

    se modifica el tirulo de los data table y se agrega card externo con clases para manejar uniformidad

--- src/components/dataTable/DataTable.jsx
+++ src/components/dataTable/DataTable.jsx
@@ -5,29 +5,29 @@ import "./dataTable.scss";
 const DataTable = ({ loading, dataSource, columns, title, id, color }) => {
   return (
     <>
-      <Table
-        id={id ? id : ""}
-        className="dataTable"
-        loading={loading ? loading : false}
-        dataSource={dataSource}
-        columns={columns}
-        title={() => (
-          <Card className={`cardTitle ${color ? color : "dark"}`}>
-            {title ? title : ""}
-          </Card>
-        )}
-        rowKey={(element) => element.id}
-        bordered
-        pagination={{
-          defaultPageSize: 10,
-          total: dataSource?.length ? dataSource.length : 0,
-          showSizeChanger: true,
-          pageSizeOptions: [5, 10, 15, 50],
-        }}
-        scroll={{
-          x: true,
-        }}
-      />
+      <Card className={"dataTableContainer"}>
+        <Card className={`cardTitle ${color ? color : "dark"}`}>
+          {title ? title : ""}
+        </Card>
+        <Table
+          id={id ? id : ""}
+          className={"dataTable"}
+          loading={loading ? loading : false}
+          dataSource={dataSource}
+          columns={columns}
+          rowKey={(element) => element.id}
+          bordered
+          pagination={{
+            defaultPageSize: 10,
+            total: dataSource?.length ? dataSource.length : 0,
+            showSizeChanger: true,
+            pageSizeOptions: [5, 10, 15, 50],
+          }}
+          scroll={{
+            x: true,
+          }}
+        />
+      </Card>
     </>
   );
 };
